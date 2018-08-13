a395dc11a1d7b3ed969e0f8e94544956dfa3e5f1
Владимир Калыний <v.kalynii@red.marketing>
2018-08-13 17:53:04 +0300

    faq desctop + правки

--- app/js/script.js
+++ app/js/script.js
@@ -49,7 +49,7 @@ $(document).ready(function(){
         }, 500);
     });
 
-
+    var topMenu = $('.faqContent__sidebarInner').offset().top;
     //Проверка при скролле страницы
     $(document).on('scroll',function(){
         //Проверяем кнопку "наверх"
@@ -57,6 +57,16 @@ $(document).ready(function(){
 
         //Анимация каринок вверх-вниз
         animateElements();
+
+
+        if($(document).scrollTop() > topMenu) {
+            $('.faqContent__sidebarInner').addClass('faqContent__sidebarInner-fixed');
+        }
+        else {
+            $('.faqContent__sidebarInner').removeClass('faqContent__sidebarInner-fixed');
+        }
+
+        onScrollFaq();
     });
 
     //Проверяем кнопку "наверх"
@@ -118,14 +128,52 @@ $(document).ready(function(){
     $('.faqSearch__input').on('focus', function (e) {
        $(this).closest('.faqSearch__inner').addClass('faqSearch__inner-focus');
     });
+
     $('.faqSearch__input').focusout('focus', function (e) {
         $(this).closest('.faqSearch__inner').removeClass('faqSearch__inner-focus');
+    });
+
+    $('.faqItems__panelTitle').on('click', function () {
+        $(this).next().slideToggle();
+        $(this).closest('.faqItems__panel').toggleClass('faqItems__panel-opened');
+    });
+
+    $(".faqContent__sidebarLink[href*=\\#]").click(function(e){
+        e.preventDefault();
+        $(document).off("scroll");
+        $(menu_selector + " a.active").removeClass("active");
+        $(this).addClass("active");
+        var hash = $(this).attr("href");
+        var target = $(hash);
+        $("html, body").animate({
+            scrollTop: target.offset().top
+        }, 500, function(){
+            window.location.hash = hash;
+            $(document).on("scroll", onScrollFaq);
+        });
     });
 
 });
 
 //Проверка при загрузке и ресайзе давать ли клас кнопке меню, и анимировать ли картинки
 $(window).on('load resize', animateElements, toggleClassLogin);
+
+
+var menu_selector = ".faqContent__sidebarInner"; // Переменная должна содержать название класса или идентификатора, обертки нашего меню.
+function onScrollFaq(){
+    var scroll_top = $(document).scrollTop();
+    $(menu_selector + " a").each(function(){
+        var hash = $(this).attr("href");
+        var target = $(hash);
+        if (target.position().top <= scroll_top && target.position().top + target.outerHeight() > scroll_top) {
+            $(menu_selector + " a.active").removeClass("active");
+            $(this).addClass("active");
+        } else {
+            $(this).removeClass("active");
+        }
+    });
+}
+
 
 //Функция анимации картинок
 function animateElements(){
